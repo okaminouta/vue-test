46b48ad04eecfaad53c516bde821706c4042e271
okaminouta <434563z@gmail.com>
2018-09-15 14:23:10 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,191 @@
+<!--npm run serve -->
+<template>
+  <div id="app">
+    <div class="header" :class='{"fixed-header": fixedHeader}'>
+      HotChocolate
+    </div>
+    <div class="w-100 content-overlay"><router-view/></div>
+
+  </div>
+</template>
+<script>
+/* ============
+ * Entry Point
+ * ============
+ *
+ * The entry point of the application.
+ */
+
+export default {
+  /**
+   * The name of the application.
+   */
+  name: 'VueBoilerplate',
+
+  /**
+   * Fires when the app has been mounted.
+   */
+  // mounted() {
+  //   // If the user is authenticated,
+  //   // fetch the data from the API
+  //   if (this.$store.state.auth.authenticated) {
+  //     this.$store.dispatch('account/find');
+  //   }
+  // },
+
+  data () {
+      return {
+          fixedHeader: false
+      };
+  },
+    methods: {
+        handleScroll () {
+            console.log(window.scrollY)
+            this.fixedHeader = window.scrollY > 100;
+        }
+    },
+    mounted () {
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    beforeDestroy () {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+};
+</script>
+
+
+<style  type="scss" lang="scss">
+  .app-container {
+    width: 70%;
+    margin: 0 auto;
+
+  }
+
+  #app {
+
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /*text-align: center;*/
+  color: #d3d3d3;
+  margin: 0;
+    padding-top: 30vh;
+  }
+  .router-view {
+    padding-top: 30vh;
+    background: white;
+  }
+
+  .content-overlay {
+    background: #fff;
+    min-height: 100vh;
+    padding: 1em;
+    -webkit-transform: translateZ(0);
+    transform: translateZ(0);
+    z-index: 10;
+  }
+
+  .header {
+    width: 100%;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    height: 30vh;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    overflow: hidden;
+    /*position: relative;*/
+    text-align: center;
+    -webkit-transform-style: preserve-3d;
+    transform-style: preserve-3d;
+    -webkit-perspective: 100px;
+    perspective: 100px;
+    font-size: 100px;
+    font-family: 'test';
+
+    overflow: hidden;
+    position: fixed;
+    right: 0;
+    text-align: center;
+    top: 0;
+    -webkit-transform-style: preserve-3d;
+    transform-style: preserve-3d;
+    z-index: 0;
+
+    -webkit-perspective: 100px;
+
+    perspective: 100px;
+    /*transition: all 500ms cubic-bezier(0.55, 0, 0.1, 1);*/
+    transition:
+                z-index .8s ease-in,
+                all 500ms ease-in;
+  }
+  .fixed-header {
+    align-items: baseline ;
+    /*text-align: left ;*/
+    /*justify-content: left ;*/
+    font-size: 45px;
+    padding-left: 20px;
+    position: fixed;
+    height: 100px;
+    z-index: 10;
+  }
+
+  .header:before {
+    -webkit-animation: fade-slide-down 2s .5s cubic-bezier(0, 0.5, 0, 1) forwards;
+    animation: fade-slide-down 2s .5s cubic-bezier(0, 0.5, 0, 1) forwards;
+    background: -webkit-linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, .8)),
+    url(assets/images/logo.jpg) no-repeat center;
+    background: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, .8)),
+    url(assets/images/logo.jpg) no-repeat center;
+    background-size: cover;
+    bottom: 0;
+    content: "";
+    left: 0;
+    opacity: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    z-index: -1;
+  }
+
+  /*<!--.header:after {-->*/
+    /*<!-- -webkit-animation: rotate-up .5s .5s cubic-bezier(0, 0.5, 0, 1) forwards;-->*/
+    /*<!--animation: rotate-up .5s .5s cubic-bezier(0, 0.5, 0, 1) forwards;-->*/
+    /*<!--background: #F9FCFF;-->*/
+    /*<!--content: "";-->*/
+    /*<!--height: 40rem;-->*/
+    /*<!--left: -5%;-->*/
+    /*<!--position: absolute;-->*/
+    /*<!--right: -5%;-->*/
+    /*<!--top: 90%;-->*/
+    /*<!-- -webkit-transform-origin: 0 0;-->*/
+    /*<!--transform-origin: 0 0;-->*/
+    /*<!--z-index: 0;-->*/
+  /*<!--}-->*/
+
+  /* Animations */
+
+  @-webkit-keyframes fade-slide-down {
+    0% {
+      opacity: 0;
+      -webkit-transform: translateY(-4rem);
+      transform: translateY(-4rem);
+    }
+    100% {
+      opacity: 1;
+      -webkit-transform: none;
+      transform: none;
+    }
+  }
+  @font-face {
+    font-family: "test";
+    src: url('assets/fonts/Gill_Sans_Nova_Semibold.otf');
+    font-weight: 400;
+    font-style: normal;
+    font-size: 100px;
+  }
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,168 @@
+<template>
+
+    <div class="card w-100" style="width: 18rem;">
+        <img class="card-img-top" :src="item.image" alt="Card image cap">
+        <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                the
+                card's content.</p>
+        </div>
+    </div>
+    <!--&lt;!&ndash;<div @click="isClickable ? onCardClick(item, type) : ''"&ndash;&gt;-->
+    <!--&lt;!&ndash;class="card w-100 d-flex"&ndash;&gt;-->
+    <!--&lt;!&ndash;:class="[{'cursor-pointer': isClickable}, type === 'Tasks::OnboardingTask' ? 'onboarding' : type]"&ndash;&gt;-->
+    <!--&lt;!&ndash;&gt;&ndash;&gt;-->
+    <!--&lt;!&ndash;&lt;!&ndash;_task_card.html.slim&ndash;&gt;&ndash;&gt;-->
+    <!--&lt;!&ndash;<div class="loader d-flex justify-content-center align-items-center" v-if="item.loading">&ndash;&gt;-->
+    <!--&lt;!&ndash;<spinner :size="50"></spinner>&ndash;&gt;-->
+    <!--&lt;!&ndash;</div>&ndash;&gt;-->
+    <!--&lt;!&ndash;<div class="card-header bg-active" v-cardHeader="type"></div>&ndash;&gt;-->
+    <!--&lt;!&ndash;<div class="card-body">&ndash;&gt;-->
+
+    <!--&lt;!&ndash;<div class="card-title">&ndash;&gt;-->
+    <!--&lt;!&ndash;<span>&ndash;&gt;-->
+    <!--&lt;!&ndash;<div v-if="item.attachment" class="nc-icon icon-vm icon-sm nc-clip"></div>&ndash;&gt;-->
+    <!--&lt;!&ndash;<img v-if="item.image" :src="item.image" alt="Icon"/>&ndash;&gt;-->
+    <!--&lt;!&ndash;<span v-if="!item.title && type === 'FollowUpTask'">Follow Up</span>&ndash;&gt;-->
+    <!--&lt;!&ndash;<span>{{item.title}}</span>&ndash;&gt;-->
+
+    <!--&lt;!&ndash;</span>&ndash;&gt;-->
+    <!--&lt;!&ndash;<div v-if="onActionsButtonClick" @click="onActionsButtonClick(item, type)" class="cursor-pointer">&ndash;&gt;-->
+    <!--&lt;!&ndash;<span class="nc-icon icon-md nc-ellipsis-v"></span>&ndash;&gt;-->
+    <!--&lt;!&ndash;</div>&ndash;&gt;-->
+    <!--&lt;!&ndash;</div>&ndash;&gt;-->
+    <!--&lt;!&ndash;<p class="card-text grow-1">&ndash;&gt;-->
+    <!--&lt;!&ndash;{{item.instructions}}&ndash;&gt;-->
+    <!--&lt;!&ndash;</p>&ndash;&gt;-->
+    <!--&lt;!&ndash;<div class="d-flex justify-content-between align-items-center task-card-footer">&ndash;&gt;-->
+
+    <!--&lt;!&ndash;<div v-if="item.driver_applicant">&ndash;&gt;-->
+    <!--&lt;!&ndash;&lt;!&ndash;<creds&ndash;&gt;&ndash;&gt;-->
+    <!--&lt;!&ndash;&lt;!&ndash;:photo="item.driver_applicant.profile_photo_path"&ndash;&gt;&ndash;&gt;-->
+    <!--&lt;!&ndash;&lt;!&ndash;:fName="item.driver_applicant.first_name"&ndash;&gt;&ndash;&gt;-->
+    <!--&lt;!&ndash;&lt;!&ndash;:lName="item.driver_applicant.last_name"&ndash;&gt;&ndash;&gt;-->
+    <!--&lt;!&ndash;&lt;!&ndash;&gt;</creds>&ndash;&gt;&ndash;&gt;-->
+    <!--&lt;!&ndash;</div>&ndash;&gt;-->
+
+    <!--&lt;!&ndash;<div v-if="type === 'report'" class="overflow-ellipsis">&ndash;&gt;-->
+    <!--&lt;!&ndash;<span class="text-muted">Last viewed:</span> {{lastViewed}}&ndash;&gt;-->
+    <!--&lt;!&ndash;&lt;!&ndash;<creds v-if="item.viewer"&ndash;&gt;&ndash;&gt;-->
+    <!--&lt;!&ndash;&lt;!&ndash;class="pl-1"&ndash;&gt;&ndash;&gt;-->
+    <!--&lt;!&ndash;&lt;!&ndash;:photo="item.viewer.profile_photo_path"&ndash;&gt;&ndash;&gt;-->
+    <!--&lt;!&ndash;&lt;!&ndash;:fName="item.viewer.first_name"&ndash;&gt;&ndash;&gt;-->
+    <!--&lt;!&ndash;&lt;!&ndash;:lName="item.viewer.last_name"&ndash;&gt;&ndash;&gt;-->
+    <!--&lt;!&ndash;&lt;!&ndash;size="small"&ndash;&gt;&ndash;&gt;-->
+    <!--&lt;!&ndash;&lt;!&ndash;&gt;</creds>&ndash;&gt;&ndash;&gt;-->
+    <!--&lt;!&ndash;</div>&ndash;&gt;-->
+
+    <!--&lt;!&ndash;<div v-if="item.due_datetime && item.status !== 'completed'"&ndash;&gt;-->
+    <!--&lt;!&ndash;:class="passDue ? 'color-grey' : 'color-accent-3'"&ndash;&gt;-->
+    <!--&lt;!&ndash;class="pull-right"&ndash;&gt;-->
+    <!--&lt;!&ndash;&gt;&ndash;&gt;-->
+    <!--&lt;!&ndash;<span> {{ item.due_datetime | moment('calendar', null, {&ndash;&gt;-->
+    <!--&lt;!&ndash;sameDay: '[Today]',&ndash;&gt;-->
+    <!--&lt;!&ndash;nextDay: '[Tomorrow]',&ndash;&gt;-->
+    <!--&lt;!&ndash;nextWeek: 'dddd',&ndash;&gt;-->
+    <!--&lt;!&ndash;lastDay: '[Yesterday]',&ndash;&gt;-->
+    <!--&lt;!&ndash;lastWeek: '[Last] dddd',&ndash;&gt;-->
+    <!--&lt;!&ndash;sameElse: 'MM/DD/YYYY'&ndash;&gt;-->
+    <!--&lt;!&ndash;})}}</span>&ndash;&gt;-->
+    <!--&lt;!&ndash;</div>&ndash;&gt;-->
+    <!--&lt;!&ndash;</div>&ndash;&gt;-->
+    <!--&lt;!&ndash;</div>&ndash;&gt;-->
+    <!--</div>-->
+</template>
+
+<script>
+    // import Creds from './userNameAndAvatar.vue'
+    export default {
+        name: 'card',
+        props: {
+            item: {type: Object}
+        },
+        data() {
+            return {};
+        },
+        computed: {
+            passDue() {
+                return this.$moment() < this.$moment(this.item.due_datetime)
+            },
+            lastViewed() {
+                return this.item.viewer ? `${this.$moment(this.item.last_viewed_at).format("MMM d, YYYY")} by` : 'N/A';
+            }
+        },
+        directives: {
+            cardHeader: {
+                inserted(el, binding, vnode) {
+                    let color = '#4db78a';
+                    switch (binding.value) {
+                        case  'Tasks::OnboardingTask':
+                            color = '#69ade6';
+                            break;
+                        case  'report':
+                            color = 'white';
+                            break;
+                    }
+                    el.style.backgroundColor = color;
+                }
+            }
+        },
+        components: {
+            // Creds,
+        }
+    };
+</script>
+
+<style scoped type="scss" lang="scss">
+    .card {
+        height: 100%;
+        opacity: .85;
+        transition: all .2s cubic-bezier(.57,.39,.43,.95),
+        height .3s cubic-bezier(0, .93, .91, 1),
+        opacity .3s cubic-bezier(0, .93, .91, 1);
+        z-index: 1;
+        img {
+            overflow: hidden;
+            height: auto;
+            max-width: 500px;
+        }
+        &:hover {
+            transform: scale(1.01);
+            height: 150%;
+            opacity: 1;
+            /*transition: height 2s ease-in;*/
+            z-index: 2;
+            -webkit-box-shadow: 0px 0px 40px 0px rgba(133, 133, 133, 1);
+            -moz-box-shadow: 0px 0px 40px 0px rgba(133, 133, 133, 1);
+            box-shadow: 0px 0px 40px 0px rgba(133, 133, 133, 1);
+            transition: all .2s cubic-bezier(.57,.39,.43,.95), z-index .3s cubic-bezier(0, .93, .91, 1),
+            height .3s cubic-bezier(0, .93, .91, 1);
+        }
+        /*height: 300px;*/
+        /*margin: 16px 0;*/
+        /*.card-body {*/
+        /*background: white;*/
+        /*p {*/
+        /*max-height: 110px;*/
+        /*overflow: hidden;*/
+        /*}*/
+        /*}*/
+        /*&:hover {*/
+        /*box-shadow: 0 2px 14px 0 rgba(0, 0, 0, 0.05);*/
+        /*}*/
+        /*}*/
+        /*.card.onboarding {*/
+        /*min-height: 0;*/
+        /*.card-body {*/
+        /*!*background: white url(~images/BlueLogo@3x.png) no-repeat -35px -10px;-->*!*/
+        /*}*/
+    }
+
+    .loader {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background: rgba(210, 215, 219, 0.2);
+        z-index: 10;
+    }
+</style>
